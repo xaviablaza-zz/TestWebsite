5c8c78a1225918f9f521bb2a46a1caf7874b2e84
Xavi Ablaza <xlablaza@gmail.com>
2015-08-28 22:36:25 -0700

    Added bootstrap and learned Blade templates

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,46 +1,15 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>test.local</title>
+@extends('master')
+@section('title', 'Home')
 
-        <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
+@section('content')
         <div class="container">
             <div class="content">
                 <div class="title">Welcome to TestWebsite.</div>
                 <div class="quote">"Free time is the enemy of progress." - Casey Neistat</div>
             </div>
         </div>
-    </body>
+        @endsection
+   
 </html>
---
+++ resources/views/master.blade.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <title> @yield('title') </title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+@include('shared.navbar')
+
+@yield('content')
+
+</body>
+</html>
