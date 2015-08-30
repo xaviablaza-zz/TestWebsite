e21b197c322cc704ccfaed07fc936e43a5d3602a
Xavi Ablaza <xlablaza@gmail.com>
2015-08-29 23:07:41 -0700

    Replaced theme with bootstrap-material-design; finished Chapter 2 of Learning Laravel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,26 @@
-<!DOCTYPE html>
-<html>
-	@extends('master')
-	@section('title', 'Home')
+@extends('master')
+@section('title', 'Home')
 
-	@section('content')
+@section('content')
+
     <div class="container">
-        <div class="content">
-            <div class="title">Welcome to TestWebsite.</div>
-            <div class="quote">"Free time is the enemy of progress." - Casey Neistat</div>
+        <div class="row banner">
+
+            <div class="col-md-12">
+
+                <h1 class="text-center margin-top-100 editContent">
+                    "Free time is the enemy of progress."
+                </h1>
+
+                <h3 class="text-center margin-top-100 editContent">- Casey Neistat</h3>
+
+<!--                 <div class="text-center"> -->
+<!--                     <img src="http://learninglaravel.net/img/LearningLaravel5_cover0.png" width="302" height="391" alt=""> -->
+<!--                 </div> -->
+
+            </div>
+
         </div>
     </div>
-    @endsection
-</html>
+
+@endsection
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,8 +1,12 @@
 <html>
 <head>
     <title> @yield('title') </title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-    <link href="/css/freelancer.css" rel="stylesheet">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Include roboto.css to use the Roboto web font, material.css to include the theme and ripples.css to style the ripple effect -->
+    <link href="/css/roboto.min.css" rel="stylesheet">
+    <link href="/css/material.min.css" rel="stylesheet">
+    <link href="/css/ripples.min.css" rel="stylesheet">
+
 </head>
 <body>
 
@@ -10,8 +14,17 @@
 
 @yield('content')
 
-<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 
+<script src="/js/ripples.min.js"></script>
+<script src="/js/material.min.js"></script>
+<script>
+    $(document).ready(function() {
+        // This command is used to initialize some elements and make them work properly
+        $.material.init();
+    });
+</script>
 </body>
+
 </html>
